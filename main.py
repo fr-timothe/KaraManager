3c803538e504e7c31ccbd75863a8e73ac37d1818
Mitsuuki06 <mitsuuki.art@gmail.com>
2024-11-30 10:37:57 +0100

    feat: :fire:

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 main=Flask(__name__)
 
 @app.route('/')
 def welcome ():
-    return "Bienvenue!"
+    return render_template('index.html')
