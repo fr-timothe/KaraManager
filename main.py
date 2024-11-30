09478e93e63eca5ab2dbf1f41f07ff71587a6781
Mitsuuki06 <mitsuuki.art@gmail.com>
2024-11-30 10:53:28 +0100

    feat: :sparkles:

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
-main = Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def welcome ():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
