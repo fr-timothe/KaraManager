75a33b6c372d936a0fe418324ae846d220ffeb3c
fr-timothe <timothe.montfrond@gmail.com>
2024-11-30 15:32:25 +0100

    feat: :art: Ajout des éléments graphique de l'index et des premières intéractions avec python et la page web

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+en_salle=10
+total_argent=3350
 
 @app.route('/')
 def welcome ():
-    return render_template('index.html')
+    return render_template('index.html', en_salle=en_salle, total_argent=total_argent)
 
 if __name__ == '__main__':
     app.run(debug=True)
